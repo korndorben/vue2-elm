e83257660b455527d197f3eb5687053a6e9941c8
张保坤 <zhangbaokun@pku.edu.cn>
2017-11-14 00:56:07 +0800

    确认订单页面

--- src/components/header/head.vue
+++ src/components/header/head.vue
@@ -1,18 +1,16 @@
 <template>
     <header id='head_top'>
-        <slot name='search'></slot>
         <section class="head_goback" v-if="goBack" @click="$router.go(-1)">
             <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.1">
-                <polyline points="12,18 4,9 12,0" style="fill:none;stroke:rgb(255,255,255);stroke-width:2"/>
+                <polyline points="12,18 4,9 12,0" style="fill:none;stroke:rgb(100,100,100);stroke-width:2"/>
             </svg>
         </section>
-        <router-link to="/profile" class="head_login">
+        <!-- <router-link to="/profile" class="head_login">
             <span class="login_span">我的</span>
-        </router-link>
+        </router-link> -->
         <section class="title_head ellipsis" v-if="headTitle">
             <span class="title_text">{{headTitle}}</span>
         </section>
-        <slot name="msite-title"></slot>
     </header>
 </template>
 
@@ -49,41 +47,27 @@
     @import '../../style/mixin';
 
     #head_top{
-        background-color: #eee;
+        background-color: #fff;
         position: fixed;
         z-index: 200;
         left: 0;
         top: 0px;
-        @include wh(100%, 1.95rem);
-        color:#fff;
+        @include wh(100%, 2.4rem);
+        border-bottom: 1px solid #efefef;
+        background-color: #fefefe;
     }
     .head_goback{
-        left: 0.4rem;
-        @include wh(0.6rem, 1rem);
-        line-height: 2.2rem;
-        margin-left: .4rem;
-    }
-    .head_login{
-        right: 0.55rem;
-        // @include sc(0.65rem, #fff);
-        @include ct;
-        .login_span{
-            // color: #fff;
-        }
-        .user_avatar{
-            // fill: #fff;
-            @include wh(.8rem, .8rem);
-        }
+        @include wh(1.2rem, 1.2rem);
+        line-height: 3rem;
+        margin-left: 1rem;
     }
     .title_head{
         @include center;
         width: 50%;
-        // color: #fff;
         text-align: center;
         .title_text{
-            @include sc(0.8rem, #fff);
+            @include sc(1rem, #555);
             text-align: center;
-            font-weight: bold;
         }
     }
 </style>
